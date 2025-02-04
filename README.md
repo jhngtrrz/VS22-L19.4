160538f5421b2ec5f3ddc0d2d21f6ebead400b99
NukeZero <29067524+NukeZero@users.noreply.github.com>
2025-02-03 22:08:23 -0500

    End of Technical Availability (ETA)

---
+++ README.md
@@ -0,0 +1,15 @@
+# [VS22] Lodelight v19.4
+
+This project originally came from Lodelight and has now reached its End of Technical Availability (ETA). It is no longer actively maintained and will remain available for archival purposes only.
+
+You can locate the file by viewing the following thread: https://flyff.one/threads/vs22-lodelight-v19-4.38. We encourage users to explore other active alternatives for future use and development.
+
+We also plan to release new bases in the future, and all updates and releases can be located on https://flyff.one.
+
+Thank you for your support!
+
+## Authors
+- [@NukeZero](https://www.github.com/nukezero)
+
+## Website
+- [Flyff.One](https://flyff.one)
---
+++ Source/Source/PatchClient/LodeConfig.h
@@ -0,0 +1,11 @@
+//|: ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+#define	NEUZ_TITLE		"[VS22] Lodelight V19.4"				//: Window title of the client.
+#define	NEUZ_BHASH		"sunkist"								//: String to boot the client.
+#define WEB_FIRST_PAGE	"http://mycoolflyff.com/index.html"		//: Loading page in patcher.
+#define SERVER_ROOT		"/RESCLIENT/"							//: Patch folder.
+#define PATCHSEVER_URL	"patcher.mycoolflyff.com"				//: Patcher domain configuration.
+#define HOME_LINK		"http://mycoolflyff.com/"				//: Link to your website.
+#define REGISTER_LINK	"http://mycoolflyff.com/register.html"	//: Link to your registration page.
+
+//|: ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
